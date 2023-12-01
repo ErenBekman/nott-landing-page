2d9bba2187d30384ac724856810d2c190a9e3567
Eren Bekman <erenbekman@hotmail.com>
2023-12-01 15:11:06 +0300

    fix 3
    Co-Authored-By: Eren Bekman <87018360+erenbekman@users.noreply.github.com>

--- components/section/Why.vue
+++ components/section/Why.vue
@@ -4,12 +4,12 @@
             <v-card-title class="mt-10">
                 <v-img :src="require('~/assets/images/people/3d-business-man-and-woman-working-with-laptop.png')" contain height="200" width="200" class="img-1" />
             </v-card-title>
-
-            <v-card-text class="d-flex flex-column justify-center align-center">
+ 
+            <v-card-text class="d-flex flex-column justify-center align-center why-title">
                 <h1 class="text-h2 font-weight-bold text-uppercase">What</h1>
                 <h2 class="text-h4 text-uppercase">We do</h2>
-                <h2 class="text-center mt-10 font-weight-medium">
-                    Nott is a <b class="whiter-text"> sustainable education platform </b> that facilitates the buying and selling of lecture notes for university students and moves their study processes to digital.
+                <h2 class="text-center mt-10 grey-text">
+                    Nott is a <b> sustainable education platform </b> that facilitates the buying and selling of lecture notes for university students and moves their study processes to digital.
                 </h2>
             </v-card-text>        
         </v-card>
@@ -19,11 +19,11 @@
                 <v-img :src="require('~/assets/images/people/3d-business-young-woman-standing-with-laptop.png')" contain height="200" width="200" class="img-2" />
             </v-card-title>
 
-            <v-card-text class="d-flex flex-column justify-center align-center">
+            <v-card-text class="d-flex flex-column justify-center align-center why-title">
                 <h1 class="text-h2 font-weight-bold text-uppercase">Why</h1>
                 <h2 class="text-h4 text-uppercase">We do it</h2>
-                <h2 class="text-center mt-10 font-weight-medium">
-                    Every year <b class="whiter-text"> 180,000 trees are sacrificed </b> for notes’ exchanges, that’s why we came up with a more sustainable & digitalized solution.
+                <h2 class="text-center mt-10 grey-text">
+                    Every year <b > 180,000 trees are sacrificed </b> for notes’ exchanges, that’s why we came up with a more sustainable & digitalized solution.
                 </h2>
             </v-card-text>
         </v-card>
@@ -33,10 +33,10 @@
                 <v-img :src="require('~/assets/images/people/business-3d-three-young-women-with-phones.png')" contain height="200" width="200" class="img-3" />
             </v-card-title>
 
-            <v-card-text class="d-flex flex-column justify-center align-center">
-                <h1 class="text-h2 font-weight-bold text-uppercase">Who</h1>
+            <v-card-text class="d-flex flex-column justify-center align-center why-title">
+                <h1 class="text-h2 font-weight-bold text-uppercase whiter-text">Who</h1>
                 <h2 class="text-h4 text-uppercase">We do it for</h2>
-                <h2 class="text-center mt-10 font-weight-medium">
+                <h2 class="text-center mt-10 grey-text">
                     For university & college students who wants to sell & buy notes for a better professional future - <b class="whiter-text"> it is for every grade. </b>
                 </h2>
             </v-card-text>
@@ -79,8 +79,19 @@ import VueScrollReveal from "scrollreveal";
 </script>
 
 <style lang="scss" scoped>
+
+.why-title {
+    font-family: 'inter';
+    color: #fff !important;
+}
+
 .whiter-text {
     color: #fff;
+}
+
+.grey-text {
+    color:#f9f9f9;
+    font-weight: 400;
 }
 
 @media only screen and (max-width: 600px) {
