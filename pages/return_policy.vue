0c325efb54e6fee16bd87acb65f6a7a90e72ab96
erenbekman <erenbekman@hotmail.com>
2024-09-08 23:42:20 +0300

    fix
    Co-Authored-By: erenbekman <87018360+erenbekman@users.noreply.github.com>

--- pages/return_policy.vue
+++ pages/return_policy.vue
@@ -66,7 +66,7 @@
     export default {
         data() {
             return {
-                pdfUrl: '/NOTT_APP_İADE_İPTAL.pdf'
+                pdfUrl: '/NOTT_APP_RETURN_POLICY.pdf'
             }
         }
     }
